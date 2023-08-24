4e1ec38a214543244ba3e361d804dc6116f49d4a
Marchukwu <nwekeamarachukwu28@gmail.com>
2023-08-24 15:50:15 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// Number variables
+let num1 = 10;
+let num2 = 5;
+
+// String variables
+let string1 = "Hello";
+let string2 = "World";
+
+// Using arithmetic operators with numbers
+let additionResult = num1 + num2;
+let subtractionResult = num1 - num2;
+let multiplicationResult = num1 * num2;
+let divisionResult = num1 / num2;
+let modulusResult = num1 % num2;
+
+console.log("Addition:", additionResult);        
+console.log("Subtraction:", subtractionResult); 
+console.log("Multiplication:", multiplicationResult); 
+console.log("Division:", divisionResult);        
+console.log("Modulus:", modulusResult);          
+
+// Using concatenation operator with strings
+let concatenatedString = string1 + " " + string2;
+console.log("Concatenated String:", concatenatedString); 
